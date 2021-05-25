7b4b0726297187686049ceedc75ddfff9c19f841
geoffop <gwpoole96@gmail.com>
2021-05-24 22:13:08 -0500

    added redis and docker compose

---
+++ app/run.py
@@ -0,0 +1,37 @@
+import os
+
+from flask import Flask, render_template, send_from_directory
+from gevent import monkey
+from gevent.pywsgi import WSGIServer
+from redis import Redis
+
+app = Flask("app")
+redis = Redis(host=os.environ["REDIS_HOST"], port=os.environ["REDIS_PORT"])
+bind_port = int(os.environ["BIND_PORT"])
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/hits")
+def hello():
+    redis.incr("hits")
+    total_hits = redis.get("hits").decode()
+    return f"Hello from Redis! I have been seen {total_hits} times."
+
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
+
+
+if __name__ == "__main__":
+    monkey.patch_all()
+    http_server = WSGIServer(("0.0.0.0", bind_port), app)
+    http_server.serve_forever()
